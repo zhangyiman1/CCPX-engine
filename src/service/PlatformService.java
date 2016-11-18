06a6049c2fdab21dc5d9d26fe094c932f81fa6f2
孙锐 <sr19930829@163.com>
2016-11-18 11:19:24 +0800

    Update PlatformService.java
    add showLatestTransaction service

--- src/service/PlatformService.java
+++ src/service/PlatformService.java
@@ -6,5 +6,6 @@ public interface PlatformService {
 	public Boolean removeExchange(String request_id, String user_from);
 	public Boolean declineExchange(String request_id, String user_to);
 	public Boolean removeOffer(String offer_id, String user_id);
+	public List<Request> showLatestTransaction(Integer sellerFrom, Integer sellerTo);
 	
 }
