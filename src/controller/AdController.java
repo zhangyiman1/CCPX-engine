d77776e14a3a07fded45de7ca0619589f68db0f3
Nutthaphon K <knutthaphon@gmail.com>
2016-11-24 22:30:59 +0800

    Update AdController.java

--- src/controller/AdController.java
+++ src/controller/AdController.java
@@ -2,100 +2,342 @@ package controller;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
+import model.ad_present;
 import model.advertisement;
 import model.seller;
+import net.sf.json.JSONArray;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
-import service.*;
+import service.AdService;
 
 @Controller
+@RequestMapping(value ="/ad")
 public class AdController {
-	@Resource(name = "AdServiceImp")
-	private AdService adservice =  new AdServiceImp();
-	 String path ;
-	 
-	 
-	@RequestMapping("/upload.do")//???
-	public String uploadAd(@RequestParam("adtitle") String adtitle,HttpServletRequest req,HttpServletResponse res,MultipartFile file, String seller_id) throws IllegalStateException, IOException
-	{
-		
-		if (adtitle.length()>40||adtitle.equals("not filled"))
-		{
-			req.setAttribute("msg", "exceed length limit40 or adtitle=null");
-		    return "uploadad";
+	@Resource(name = "adServiceImp")
+	private AdService adServiceImp;
+			
+	
+@RequestMapping("getAd")
+@ResponseBody
+	public void getAd(HttpServletRequest req, HttpServletResponse res) {
+		List<ad_present> advertisement_list = adServiceImp.getAd();
+		JSONArray json = JSONArray.fromObject(advertisement_list);
+        System.out.print(json);
+        System.out.close();
+        PrintWriter out =null;
+        
+        try{
+        out = res.getWriter();
+        out.write(json.toString());
+        out.flush();
+        out.close();
+        }catch (IOException e) {  
+            e.printStackTrace();  
+        } finally {  
+            if (out != null) {  
+                out.close();  
+            } 
+        } 
+	}
+
+@RequestMapping("getAdBySellerID")
+@ResponseBody
+	public void getAdBySellerID(HttpServletRequest req, HttpServletResponse res, String id) {
+		List<advertisement> advertisement_list = adServiceImp.getAdBySellerID(id);
+		JSONArray json = JSONArray.fromObject(advertisement_list);
+        System.out.print(json);
+        System.out.close();
+        PrintWriter out =null;
+        
+        try{
+        out = res.getWriter();
+        out.write(json.toString());
+        out.flush();
+        out.close();
+        }catch (IOException e) {  
+            e.printStackTrace();  
+        } finally {  
+            if (out != null) {  
+                out.close();  
+            } 
+        } 
+	}
+
+@RequestMapping("addAd")
+@ResponseBody
+	public void addAd(HttpServletRequest req, HttpServletResponse res, String id, String title, MultipartFile image) {
+	
+	int num = adServiceImp.getNumberOfAdBySellerID(id);
+	res.setCharacterEncoding("UTF-8"); 
+    res.setContentType("text/json");
+    PrintWriter out =null;
+    
+	if(num>=3){
+		String message = "more_than_three";
+		        try{
+		        out = res.getWriter();
+		        out.write(message);
+		        out.flush();
+		        out.close();
+		        }catch (IOException e) {  
+		            e.printStackTrace();  
+		        } finally {  
+		            if (out != null) {  
+		                out.close();  
+		            } 
+		        } 
+	}else{
+		    String path = null;
+	        String type = null;
+	        String fileName = image.getOriginalFilename();
+	        type=fileName.indexOf(".")!=-1?fileName.substring(fileName.lastIndexOf(".")+1, fileName.length()):null;
+		  if (image.getSize()<2000000) {
+         	 type=fileName.indexOf(".")!=-1?fileName.substring(fileName.lastIndexOf(".")+1, fileName.length()):null;
+         	 if (type!=null) {
+         		 if ("PNG".equals(type.toUpperCase())||"JPG".equals(type.toUpperCase())) {//图片格式正确，执行添加广告操作
+         			String realPath=req.getSession().getServletContext().getRealPath("/")+"images/";
+		                String trueFileName="ad"+id+"_"+String.valueOf(num+1)+"."+type;
+		                path=realPath+trueFileName;
+		                try{
+		                	image.transferTo(new File(path));}
+		                catch (IOException e) {  
+	       		            e.printStackTrace();  
+	       		        } finally {  
+	       		        } 
+		                advertisement newad = new advertisement();
+		                newad.setAdvertisement_Image("images/"+trueFileName);
+		                newad.setAdvertisement_Title(title);
+		                newad.setSeller_id(Integer.parseInt(id));
+		                boolean b = adServiceImp.addAd(newad);
+		               if (b) {
+           			    String message = "success";
+           		        try{
+           		        out = res.getWriter();
+           		        out.write(message);
+           		        out.flush();
+           		        out.close();
+           		        }catch (IOException e) {  
+           		            e.printStackTrace();  
+           		        } finally {  
+           		            if (out != null) {  
+           		                out.close();  
+           		            } 
+           		        } 
+           		        
+           		} else {
+           			    String message = "false_exception";  
+           		        try{
+           		        out = res.getWriter();
+           		        out.write(message);
+           		        out.flush();
+           		        out.close();
+           		        }catch (IOException e) {  
+           		            e.printStackTrace();  
+           		        } finally {  
+           		            if (out != null) {  
+           		                out.close();  
+           		            }//if 
+           		        } //finally
+           		}//else
+         		 }//图片格式正确，执行regist操作
+         		 else{//图片格式不正确
+         			    String message = "false_format_not_correct";  
+	           		        try{
+	           		        out = res.getWriter();
+	           		        out.write(message);
+	           		        out.flush();
+	           		        out.close();
+	           		        }catch (IOException e) {  
+	           		            e.printStackTrace();  
+	           		        } finally {  
+	           		            if (out != null) {  
+	           		                out.close();  
+	           		            }//if 
+	           		        } //finally
+         		 }//图片格式不正确
+         	 }//type!=null
+         	 else{//type==null
+         		 String message = "false_type_null";  
+           		        try{
+           		        out = res.getWriter();
+           		        out.write(message);
+           		        out.flush();
+           		        out.close();
+           		        }catch (IOException e) {  
+           		            e.printStackTrace();  
+           		        } finally {  
+           		            if (out != null) {  
+           		                out.close();  
+           		            }//if 
+           		        } //finally
+         	 }//type!==null
+         }
+         else{//Logo.getSize()>2000000
+         	String message = "false_size_too_big";  
+    		        try{
+    		        out = res.getWriter();
+    		        out.write(message);
+    		        out.flush();
+    		        out.close();
+    		        }catch (IOException e) {  
+    		            e.printStackTrace();  
+    		        } finally {  
+    		            if (out != null) {  
+    		                out.close();  
+    		            }//if 
+    		        } //finally
+         }//Logo.getSize()>2000000
+	}
+	
+	
+	}
+
+
+
+@RequestMapping("editAd")
+@ResponseBody
+	public void aditAd(HttpServletRequest req, HttpServletResponse res, String id, String title, MultipartFile image) {
+	
+	res.setCharacterEncoding("UTF-8"); 
+    res.setContentType("text/json");
+    PrintWriter out =null;
+    advertisement ad =  adServiceImp.getAdByAdID(id);
+	if(image==null){//未上传图片
+		ad.setAdvertisement_Title(title);
+		boolean b = adServiceImp.editAdd(ad);
+		String message =null;
+		if(b){
+		message = "success";
+		}else{
+		message = "system_exception";
 		}
-			
-		advertisement ad=adservice.checkAd(adtitle);
-		
-		if(ad!=null)
-		{
-			req.setAttribute("msg", "repeated adtitle");
-		    return "uploadad";
-		}
-		
-		 if (file.getSize()<2000000) {// 判断上传的文件是否为空
-	            String path=null;// 文件路径
-	            String type=null;// 文件类型
-	            String fileName=file.getOriginalFilename();// 文件原名称
-	            System.out.println("上传的文件原名称:"+fileName);
-	            // 判断文件类型
-	            type=fileName.indexOf(".")!=-1?fileName.substring(fileName.lastIndexOf(".")+1, fileName.length()):null;
-	            if (type!=null) {// 判断文件类型是否为空
-	                if ("PNG".equals(type.toUpperCase())||"JPG".equals(type.toUpperCase())) {
-	                    // 项目在容器中实际发布运行的根路径
-	                    String realPath=req.getSession().getServletContext().getRealPath("/")+"images\\";
-	                    // 自定义的文件名称
-	                    String trueFileName=adtitle+"."+type;
-	                    // 设置存放图片文件的路径
-	                    path=realPath+/*System.getProperty("file.separator")+*/trueFileName;
-	                    System.out.println("存放图片文件的路径:"+path);
-	                    // 转存文件到指定的路径
-	                    file.transferTo(new File(path));
-	                    System.out.println("文件成功上传到指定目录下");
-	                    System.out.println(file.getSize());
-	                    this.path = path;
-	                }else {
-	                    System.out.println("不是我们想要的文件类型,请按要求重新上传");
-	                    req.setAttribute("msg", "not correct type(png,jpg)");
-	                    return "uploadad";
-	                }
-	            }else {
-	                System.out.println("文件类型为空");
-	                req.setAttribute("msg", "type cant = null");
-	                return "uploadad";
-	            }
-	        }else {
-//	            System.out.println("没有找到相对应的文件");
-//	            req.setAttribute("msg", "file=null");
-	        	System.out.println("文件太大");
-                req.setAttribute("msg", "file bigger than 2mb");
-	            return "uploadad";}
-		
-			
-			
-		seller seller = new seller();
-		seller.setSeller_id(Integer.parseInt(seller_id));
-		System.out.println(seller.getSeller_id());
-//    	seller.setSeller_id(1);
-//		seller.setSeller_Name("apple");
-		
-		
-		adservice.uploadAd(seller,adtitle,path);
-		req.setAttribute("msg", "upload success");
-	    return "uploadad";
-		
+		        try{
+		        out = res.getWriter();
+		        out.write(message);
+		        out.flush();
+		        out.close();
+		        }catch (IOException e) {  
+		            e.printStackTrace();  
+		        } finally {  
+		            if (out != null) {  
+		                out.close();  
+		            } 
+		        } 
+	}else{//上传了图片
+		    String path = null;
+	        String type = null;
+	        String fileName = image.getOriginalFilename();
+	        type=fileName.indexOf(".")!=-1?fileName.substring(fileName.lastIndexOf(".")+1, fileName.length()):null;
+		  if (image.getSize()<2000000) {
+         	 type=fileName.indexOf(".")!=-1?fileName.substring(fileName.lastIndexOf(".")+1, fileName.length()):null;
+         	 if (type!=null) {
+         		 if ("PNG".equals(type.toUpperCase())||"JPG".equals(type.toUpperCase())) {//图片格式正确，执行修改广告操作
+         			    String realPath=req.getSession().getServletContext().getRealPath("/")+"/";
+         			    String oldimage = ad.getAdvertisement_Image();
+         			    File f = new File(realPath+oldimage);
+         	            f.delete();
+		                String trueFileName=oldimage.substring(0, oldimage.indexOf("."))+"."+type;
+		                path=realPath+trueFileName;
+		                try{
+		                	image.transferTo(new File(path));}
+		                catch (IOException e) {  
+	       		            e.printStackTrace();  
+	       		        } finally {  
+	       		        } 
+		                ad.setAdvertisement_Title(title);
+		                ad.setAdvertisement_Image(trueFileName);
+		                boolean b = adServiceImp.editAdd(ad);
+		               if (b) {
+           			    String message = "success";
+           		        try{
+           		        out = res.getWriter();
+           		        out.write(message);
+           		        out.flush();
+           		        out.close();
+           		        }catch (IOException e) {  
+           		            e.printStackTrace();  
+           		        } finally {  
+           		            if (out != null) {  
+           		                out.close();  
+           		            } 
+           		        } 
+           		        
+           		} else {
+           			    String message = "false_exception";  
+           		        try{
+           		        out = res.getWriter();
+           		        out.write(message);
+           		        out.flush();
+           		        out.close();
+           		        }catch (IOException e) {  
+           		            e.printStackTrace();  
+           		        } finally {  
+           		            if (out != null) {  
+           		                out.close();  
+           		            }//if 
+           		        } //finally
+           		}//else
+         		 }//图片格式正确，执行regist操作
+         		 else{//图片格式不正确
+         			    String message = "false_format_not_correct";  
+	           		        try{
+	           		        out = res.getWriter();
+	           		        out.write(message);
+	           		        out.flush();
+	           		        out.close();
+	           		        }catch (IOException e) {  
+	           		            e.printStackTrace();  
+	           		        } finally {  
+	           		            if (out != null) {  
+	           		                out.close();  
+	           		            }//if 
+	           		        } //finally
+         		 }//图片格式不正确
+         	 }//type!=null
+         	 else{//type==null
+         		 String message = "false_type_null";  
+           		        try{
+           		        out = res.getWriter();
+           		        out.write(message);
+           		        out.flush();
+           		        out.close();
+           		        }catch (IOException e) {  
+           		            e.printStackTrace();  
+           		        } finally {  
+           		            if (out != null) {  
+           		                out.close();  
+           		            }//if 
+           		        } //finally
+         	 }//type!==null
+         }
+         else{//Logo.getSize()>2000000
+         	String message = "false_size_too_big";  
+    		        try{
+    		        out = res.getWriter();
+    		        out.write(message);
+    		        out.flush();
+    		        out.close();
+    		        }catch (IOException e) {  
+    		            e.printStackTrace();  
+    		        } finally {  
+    		            if (out != null) {  
+    		                out.close();  
+    		            }//if 
+    		        } //finally
+         }//Logo.getSize()>2000000
+	}
+	
+	
 	}
 
 }
