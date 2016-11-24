8064b730fd6506b557091ab8acb0bbe46a53dcbe
Nutthaphon K <knutthaphon@gmail.com>
2016-11-24 22:29:03 +0800

    Update SellerManagementController.java

--- src/controller/SellerManagementController.java
+++ src/controller/SellerManagementController.java
@@ -1,15 +1,15 @@
 package controller;
 
-import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+
 import javax.annotation.Resource;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import model.seller;
 import net.sf.json.JSONObject;
 
@@ -17,10 +17,10 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import service.SellerManagementService;
 import utils.MD5Util;
-import utils.VerifyCode;
 
 @Controller
 @RequestMapping(value ="/seller")
@@ -47,11 +47,14 @@ public class SellerManagementController {
 	        
 			if (Seller != null) {
 				//req.getSession().setAttribute("Seller", Seller);
-		        String message = "{'message':'success'}";  
-		        JSONObject json = JSONObject.fromObject(message);
+				//Cookie cookie=new Cookie("sellerid",String.valueOf(Seller.getSeller_id()));
+				//res.addCookie(cookie); {'message':'success','sellerid':'sellerid','sellername':'sellername'}
+		        String sellerid = String.valueOf(Seller.getSeller_id());
+		        String sellername = Seller.getSeller_Name();
+		        String message = "{'message':'success','sellerid':'"+sellerid+"','sellername':'"+sellername+"'}";
+		        JSONObject json = JSONObject.fromObject(message);      
 		        System.out.print(json);
 		        System.out.close();
-		        
 		        try{
 		        out = res.getWriter();
 		        out.write(json.toString());
@@ -86,7 +89,121 @@ public class SellerManagementController {
 			}
 
 	}
+	
+	@RequestMapping("checkActivationCode")
+	@ResponseBody
+	public void checkActivationCode(HttpServletRequest req, HttpServletResponse res,
+			String code, String sellerid) {
+			boolean b = sellerManagementServiceImp.checkActivationCode(code,sellerid);
+			res.setCharacterEncoding("UTF-8"); 
+	        res.setContentType("text/json");
+	        PrintWriter out =null;
+			if (b) {
+				//修改Seller Status
+				boolean r = sellerManagementServiceImp.updateSellerStatus(sellerid);
+				
+				if(r){
+		        String message = "{'message':'success'}";	        
+		        JSONObject json = JSONObject.fromObject(message);      
+		        System.out.print(json);
+		        System.out.close();
+		        try{
+		        out = res.getWriter();
+		        out.write(json.toString());
+		        out.flush();
+		        out.close();
+		        }catch (IOException e) {  
+		            e.printStackTrace();  
+		        } finally {  
+		            if (out != null) {  
+		                out.close();  
+		            } 
+		        } 
+				}else{
+					String message = "{'message':'You are already a member'}";  
+			        JSONObject json = JSONObject.fromObject(message);
+			        System.out.print(json);
+			        System.out.close();
+			        try{
+			        out = res.getWriter();
+			        out.write(json.toString());
+			        out.flush();
+			        out.close();
+			        }catch (IOException e) {  
+			            e.printStackTrace();  
+			        } finally {  
+			            if (out != null) {  
+			                out.close();  
+			            } 
+			        } 
+				}
+				
+			}else {
+		        String message = "{'message':'activation code is not correct'}";  
+		        JSONObject json = JSONObject.fromObject(message);
+		        System.out.print(json);
+		        System.out.close();
+		        try{
+		        out = res.getWriter();
+		        out.write(json.toString());
+		        out.flush();
+		        out.close();
+		        }catch (IOException e) {  
+		            e.printStackTrace();  
+		        } finally {  
+		            if (out != null) {  
+		                out.close();  
+		            } 
+		        } 
+			}
 
+	}
+
+	@RequestMapping("/validateSeller_Username")
+	@ResponseBody
+	public void validateSeller_Username(HttpServletRequest req,
+			HttpServletResponse res, String username){
+		seller Seller = sellerManagementServiceImp.validateUsername(username);
+		res.setCharacterEncoding("UTF-8"); 
+        res.setContentType("text/json");
+        PrintWriter out =null;
+		if (Seller != null) {
+	        String message = "{'message':'exist'}";  
+	        JSONObject json = JSONObject.fromObject(message);
+	        System.out.print(json);
+	        System.out.close();
+	        try{
+	        out = res.getWriter();
+	        out.write(json.toString());
+	        out.flush();
+	        out.close();
+	        }catch (IOException e) {  
+	            e.printStackTrace();  
+	        } finally {  
+	            if (out != null) {  
+	                out.close();  
+	            } 
+	        } 		
+		} else {
+	        String message = "{'message':'not exist'}";  
+	        JSONObject json = JSONObject.fromObject(message);
+	        System.out.print(json);
+	        System.out.close();
+	        try{
+	        out = res.getWriter();
+	        out.write(json.toString());
+	        out.flush();
+	        out.close();
+	        }catch (IOException e) {  
+	            e.printStackTrace();  
+	        } finally {  
+	            if (out != null) {  
+	                out.close();  
+	            } 
+	        } 
+		}
+		}
+	
 	@RequestMapping("/exit")
 	@ResponseBody
 	public void exit(HttpServletRequest req) {
@@ -98,58 +215,134 @@ public class SellerManagementController {
 	}
 	
 	@RequestMapping("/registSeller")
-	public void registSeller(HttpServletRequest req, HttpServletResponse res, String Seller_Name,
-			String Seller_Address, String Seller_Telephone, String Seller_Email, String Seller_Username,
-			String Seller_Password, String IndustryType_id, String Seller_Logo,
-			String Seller_Description) {
+	public void registSeller(HttpServletRequest req, HttpServletResponse res, String seller_Name,
+			String seller_Address, String seller_Telephone, String seller_Email, String seller_Username,
+			String seller_Password, String industryType_id, MultipartFile Logo,
+			String seller_Description){
 		
-		seller Seller = new seller();
-		Seller.setSeller_Name(Seller_Name);
-		Seller.setSeller_Address(Seller_Address);
-		Seller.setSeller_Telephone(Seller_Telephone);
-		Seller.setSeller_Email(Seller_Email);
-		Seller.setSeller_Username(Seller_Username);
-		Seller.setSeller_Password(Seller_Password);
-		Seller.setIndustryType_id(IndustryType_id);
-		Seller.setSeller_Logo(Seller_Logo);
-		Seller.setSeller_Description(Seller_Description);
-		boolean b = sellerManagementServiceImp.regist(Seller);
-		 PrintWriter out =null;
-		if (b) {
-			    String message = "{'message':'success'}";  
-		        JSONObject json = JSONObject.fromObject(message);
-		        System.out.print(json);
-		        System.out.close();
-		        try{
-		        out = res.getWriter();
-		        out.write(json.toString());
-		        out.flush();
-		        out.close();
-		        }catch (IOException e) {  
-		            e.printStackTrace();  
-		        } finally {  
-		            if (out != null) {  
-		                out.close();  
-		            } 
-		        } 
-		} else {
-			 String message = "{'message':'false'}";  
-		        JSONObject json = JSONObject.fromObject(message);
-		        System.out.print(json);
-		        System.out.close();
-		        try{
-		        out = res.getWriter();
-		        out.write(json.toString());
-		        out.flush();
-		        out.close();
-		        }catch (IOException e) {  
-		            e.printStackTrace();  
-		        } finally {  
-		            if (out != null) {  
-		                out.close();  
-		            } 
-		        } 
-		}
+		    seller Seller = new seller();
+		    Seller.setSeller_Name(seller_Name);
+		    Seller.setSeller_Address(seller_Address);
+		    Seller.setSeller_Telephone(seller_Telephone);
+		    Seller.setSeller_Email(seller_Email);
+	    	Seller.setSeller_Username(seller_Username);
+		    Seller.setSeller_Password(seller_Password);
+	     	Seller.setIndustryType_id(industryType_id);
+	    	Seller.setSeller_Description(seller_Description);
+	    	Seller.setSeller_Status("0");
+		    PrintWriter out =null;
+	     	res.setCharacterEncoding("UTF-8"); 
+            res.setContentType("text/json");
+        
+	        String path = null;
+	        String type = null;
+	        String fileName = Logo.getOriginalFilename();
+	        type=fileName.indexOf(".")!=-1?fileName.substring(fileName.lastIndexOf(".")+1, fileName.length()):null;
+	        
+
+	                if (Logo.getSize()<2000000) {
+	                	 type=fileName.indexOf(".")!=-1?fileName.substring(fileName.lastIndexOf(".")+1, fileName.length()):null;
+	                	 if (type!=null) {
+	                		 if ("PNG".equals(type.toUpperCase())||"JPG".equals(type.toUpperCase())) {//图片格式正确，执行regist操作
+	                			String realPath=req.getSession().getServletContext().getRealPath("/")+"images/";
+	     		                String trueFileName=seller_Username+"."+type;
+	     		                path=realPath+trueFileName;
+	     		                System.out.println("存放图片文件的路径:"+path);
+	     		                try{
+	     		                Logo.transferTo(new File(path));}
+	     		                catch (IOException e) {  
+	     	       		            e.printStackTrace();  
+	     	       		        } finally {  
+	     	       		        } 
+	     		                Seller.setSeller_Logo("images/"+trueFileName);
+	     		                boolean b = sellerManagementServiceImp.regist(Seller);
+	     		               if (b) {
+	   	           			    String message = "success";
+	   	           		        try{
+	   	           		        out = res.getWriter();
+	   	           		        out.write(message);
+	   	           		        out.flush();
+	   	           		        out.close();
+	   	           		        }catch (IOException e) {  
+	   	           		            e.printStackTrace();  
+	   	           		        } finally {  
+	   	           		            if (out != null) {  
+	   	           		                out.close();  
+	   	           		            } 
+	   	           		        } 
+	   	           		        
+	   	           		} else {
+	   	           			    String message = "false_exception";  
+	   	           		        try{
+	   	           		        out = res.getWriter();
+	   	           		        out.write(message);
+	   	           		        out.flush();
+	   	           		        out.close();
+	   	           		        }catch (IOException e) {  
+	   	           		            e.printStackTrace();  
+	   	           		        } finally {  
+	   	           		            if (out != null) {  
+	   	           		                out.close();  
+	   	           		            }//if 
+	   	           		        } //finally
+	   	           		}//else
+	                		 }//图片格式正确，执行regist操作
+	                		 else{//图片格式不正确
+	                			    String message = "false_format_not_correct";  
+		   	           		        try{
+		   	           		        out = res.getWriter();
+		   	           		        out.write(message);
+		   	           		        out.flush();
+		   	           		        out.close();
+		   	           		        }catch (IOException e) {  
+		   	           		            e.printStackTrace();  
+		   	           		        } finally {  
+		   	           		            if (out != null) {  
+		   	           		                out.close();  
+		   	           		            }//if 
+		   	           		        } //finally
+	                		 }//图片格式不正确
+	                	 }//type!=null
+	                	 else{//type==null
+	                		 String message = "false_type_null";  
+	   	           		        try{
+	   	           		        out = res.getWriter();
+	   	           		        out.write(message);
+	   	           		        out.flush();
+	   	           		        out.close();
+	   	           		        }catch (IOException e) {  
+	   	           		            e.printStackTrace();  
+	   	           		        } finally {  
+	   	           		            if (out != null) {  
+	   	           		                out.close();  
+	   	           		            }//if 
+	   	           		        } //finally
+	                	 }//type!==null
+	                }
+	                else{//Logo.getSize()>2000000
+	                	String message = "false_size_too_big";  
+	           		        try{
+	           		        out = res.getWriter();
+	           		        out.write(message);
+	           		        out.flush();
+	           		        out.close();
+	           		        }catch (IOException e) {  
+	           		            e.printStackTrace();  
+	           		        } finally {  
+	           		            if (out != null) {  
+	           		                out.close();  
+	           		            }//if 
+	           		        } //finally
+	                }//Logo.getSize()>2000000
+		               
+	                	
+	                	
+	                	
+	                	
+	                	
+	                	
+		
+		
 	}
 
 	@RequestMapping("/updateSellerinfo")
