07996447426749512ea63c2bed2426b0f58558f5
wangxiajing <377978850@qq.com>
2016-11-28 09:50:41 +0800

    connection with  blockchain
    use post to connect with blockchain

---
+++ PlatformService.java
@@ -0,0 +1,40 @@
+package service;
+
+import java.util.List;
+
+import model.Notification;
+import model.Offer;
+import model.Request;
+
+public interface PlatformService {
+
+	public Boolean removeExchange(Integer request_id, Integer user_from);
+
+	public Boolean declineExchange(Integer request_id, Integer user_to);
+
+	public Boolean removeOffer(Integer offer_id, Integer user_id);
+
+	public List<Request> showLatestTransaction(Integer sellerFrom, Integer sellerTo);
+
+	public List<Offer> searchExcahnge(Integer sellerFrom, Integer sellerTo, Integer pointsFrom, Integer pointsToMin);
+
+	public List<Offer> showRecommendationList(Integer sellerFrom, Integer sellerTo, Integer pointsFrom,
+			Integer pointsToMin);
+
+	public Boolean updateOfferStatus(Integer offer_id, Integer user_from);
+
+	public Boolean updateRequestStatus(Integer request_id, Integer user_from);
+
+	public Boolean acceptRequest(Integer request_id);
+
+	public Boolean createNotification(Integer userId, Integer status, Integer eR_ID);
+	
+	public boolean sendExchangeToBlockChain(Integer Request_id, Integer userFrom, Integer userTo, Integer sellerFrom, Integer sellerTo, Integer pointFrom, Integer pointTo);
+
+	
+	//NOTIFACTION TESTPAGE
+	public List<Notification> NotifListsByUserId(Integer userId);
+	public List<Notification> getNotifUnread(Integer userId);
+
+	
+}
